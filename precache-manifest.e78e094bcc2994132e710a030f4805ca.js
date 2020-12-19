20293be0c09e0e6266e6b57c5cfe92ed6a1689ad
developer-73 <priti.khattar@gmail.com>
2020-12-19 22:01:40 +0530

    Updates

---
+++ precache-manifest.e78e094bcc2994132e710a030f4805ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b9bc01ae99c6eb91949ce5944f5195",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "7cc0b59746b8020c6f40",
+    "url": "/movie-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "236bb1cf03a3daf5b639",
+    "url": "/movie-app/static/css/main.22e4f564.chunk.css"
+  },
+  {
+    "revision": "7cc0b59746b8020c6f40",
+    "url": "/movie-app/static/js/2.19854002.chunk.js"
+  },
+  {
+    "revision": "c43cdb876cce9342f2faee451e84a2b7",
+    "url": "/movie-app/static/js/2.19854002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236bb1cf03a3daf5b639",
+    "url": "/movie-app/static/js/main.36a057ce.chunk.js"
+  },
+  {
+    "revision": "cf013d390c273dc2c2c0",
+    "url": "/movie-app/static/js/runtime-main.ae3225eb.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/movie-app/static/media/loader.26ef8bb4.gif"
+  },
+  {
+    "revision": "e58a3e3dc38731e969840b7d3e001beb",
+    "url": "/movie-app/static/media/logo2.e58a3e3d.jpg"
+  }
+]);
